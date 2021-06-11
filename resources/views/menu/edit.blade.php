d6891ad93b7796e4d2b91838c3fbe4ae60da8a0a
deanamilushmajid@gmail.com <deanamilush@gmail.com>
2021-06-11 13:50:55 +0700

    update checkbox state

--- resources/views/menu/edit.blade.php
+++ resources/views/menu/edit.blade.php
@@ -4,20 +4,20 @@
 
 @section('container-fluid')
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Kantin</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/kantin">Home</a>
-        </li>
-      </ul>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Kantin</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/kantin">Home</a>
+                </li>
+            </ul>
+        </div>
     </div>
-  </div>
-</nav>  
+</nav>
 @endsection
 
 @section('container')
@@ -26,66 +26,63 @@
         <div class="col-10">
             <h2 class="mt-2">Edit Master Menu</h2>
             <form method="POST" action="{{ route('menu.update', $menu->id) }}">
-            @method('patch')
-            @csrf
-            <div class="mb-3">
+                @method('patch')
+                @csrf
+                <div class="mb-3">
                     <label for="kode_katering" class="form-label">Kode Katering</label>
-                    <input type="text" name="kode_katering" class="form-control" value="{{$menu->kode_katering}}" id="kode_katering" >
+                    <input type="text" name="kode_katering" class="form-control" value="{{$menu->kode_katering}}" id="kode_katering">
                 </div>
                 <div class="mb-3">
-                <label class="form-label">Hari</label>
-                    <br/>
+                    <label class="form-label">Hari</label><br />
+                    <?php
+                    $senin = '';
+                    $selasa = '';
+                    $rabu = '';
+                    $kamis = '';
+                    $jumat = '';
+                    $sabtu = '';
+                    ?>
+
                     @foreach($menu->hari as $value)
-                    <p>{{$menu['hari']}}</p>
-                    <div class="form-check form-check-inline">
-                        @if ($value === 'senin')
-                        <input class="form-check-input" type="checkbox" name="hari[]"  value="$value" checked>
-                        @else
-                        <input class="form-check-input" type="checkbox" name="hari[]"  value="senin">
-                        @endif
-                        <label class="form-check-label" >Senin</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                        @if ($value === 'selasa')
-                        <input class="form-check-input" type="checkbox" name="hari[]"  value="$value" checked>
-                        @else
-                        <input class="form-check-input" type="checkbox" name="hari[]" value="selasa">
-                        @endif
-                        <label class="form-check-label" >Selasa</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                        @if ($value === 'rabu')
-                        <input class="form-check-input" type="checkbox" name="hari[]"  value="$value" checked>
-                        @else
-                        <input class="form-check-input" type="checkbox" name="hari[]"  value="rabu">
-                        @endif
-                        <label class="form-check-label" >Rabu</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                        @if ($value === 'kamis')
-                        <input class="form-check-input" type="checkbox" name="hari[]"  value="$value" checked>
-                        @else
-                        <input class="form-check-input" type="checkbox" name="hari[]" value="kamis">
-                        @endif
-                        <label class="form-check-label" >Kamis</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                        @if ($value === 'jumat')
-                        <input class="form-check-input" type="checkbox" name="hari[]"  value="$value" checked>
-                        @else
-                        <input class="form-check-input" type="checkbox" name="hari[]" value="jumat">
-                        @endif
-                        <label class="form-check-label" >Jumat</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                        @if ($value === 'sabtu')
-                        <input class="form-check-input" type="checkbox" name="hari[]"  value="$value" checked>
-                        @else
-                        <input class="form-check-input" type="checkbox" name="hari[]" value="sabtu">
-                        @endif
-                        <label class="form-check-label" >Sabtu</label>
-                    </div>
+                    @if($value == 'Senin')
+                    @php $senin = $value @endphp
+                    @elseif($value == 'Selasa')
+                    @php $selasa = $value @endphp
+                    @elseif($value == 'Rabu')
+                    @php $rabu = $value @endphp
+                    @elseif($value == 'Kamis')
+                    @php $kamis = $value @endphp
+                    @elseif($value == 'Jumat')
+                    @php $jumat = $value @endphp
+                    @elseif($value == 'Sabtu')
+                    @php $sabtu = $value @endphp
+                    @endif
                     @endforeach
+
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" name="hari[]" value="Senin" {{$senin == 'Senin' ? 'checked' : null}}>
+                        <label class="form-check-label">Senin</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" name="hari[]" value="Selasa" {{$selasa == 'Selasa' ? 'checked' : null}}>
+                        <label class="form-check-label">Selasa</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" name="hari[]" value="Rabu" {{$rabu == 'Rabu' ? 'checked' : null}}>
+                        <label class="form-check-label">Rabu</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" name="hari[]" value="Kamis" {{$kamis == 'Kamis' ? 'checked' : null}}>
+                        <label class="form-check-label">Kamis</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" name="hari[]" value="Jumat" {{$jumat == 'Jumat' ? 'checked' : null}}>
+                        <label class="form-check-label">Jumat</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" name="hari[]" value="Sabtu" {{$sabtu == 'Sabtu' ? 'checked' : null}}>
+                        <label class="form-check-label">Sabtu</label>
+                    </div>
                 </div>
                 <div class="mb-3">
                     <label for="menu_utama" class="form-label">Menu Utama</label>
@@ -100,7 +97,7 @@
                     <input type="text" name="harga_add_on" class="form-control" value="{{$menu->harga_add_on}}" id="harga_add_on">
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
-            </form> 
+            </form>
         </div>
     </div>
 </div>
