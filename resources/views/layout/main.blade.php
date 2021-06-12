2d13058d79baeffa307211e518bfc9a767e99c60
deanamilushmajid@gmail.com <deanamilush@gmail.com>
2021-06-12 13:51:12 +0700

    solved add jquery

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1,35 +1,35 @@
 <!DOCTYPE html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        
-        <!-- Include the plugin's CSS and JS: -->
-        <link rel="stylesheet" href="searchableOptionList.css">
-        <script src="{{asset('js/jquery-3.6.0.min.js')}}"></script>
-        <script src="{{asset('js/sol.js')}}"></script>
-        <script src="{{asset('css/sol.css')}}"></script>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 
-        <title>@yield('title')</title>
-    </head>
+    <!-- Include the plugin's CSS and JS: -->
+    <link rel="stylesheet" href="searchableOptionList.css">
+    <script src="{{asset('js/jquery-3.6.0.min.js')}}"></script>
+    <script src="{{asset('js/sol.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('css/sol.css')}}">
 
-    <body>
-        @yield('container-fluid')
-        @yield('container')
-        
-        <script type="text/javascript">
-                    $(function() {
-                        $('#select-test').searchableOptionList();
-                    });    
-        </script>
+    <title>@yield('title')</title>
+</head>
 
-    </body>   
+<body>
+    @yield('container-fluid')
+    @yield('container')
+
+    <script type="text/javascript">
+        $(function() {
+            // initialize sol
+            $('#my-select').searchableOptionList();
+        });
+    </script>
+
+</body>
 
 </html>
--- resources/views/menu/create.blade.php
+++ resources/views/menu/create.blade.php
@@ -4,20 +4,20 @@
 
 @section('container-fluid')
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Kantin</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/kantin">Home</a>
-        </li>
-      </ul>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Kantin</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/kantin">Home</a>
+                </li>
+            </ul>
+        </div>
     </div>
-  </div>
-</nav>  
+</nav>
 @endsection
 
 @section('container')
@@ -25,38 +25,58 @@
     <div class="row">
         <div class="col-10">
             <h2 class="mt-2">Tambah Master Menu</h2>
-            <form method="POST" action="{{ route('menu.store') }}"  enctype="multipart/form-data">
-            @csrf
-            <div class="mb-3">
+            <form method="POST" action="{{ route('menu.store') }}" enctype="multipart/form-data">
+                @csrf
+
+                <select id="my-select" name="character" multiple="multiple">
+                    <optgroup label="The Griffins">
+                        <option value="Peter">Peter Griffin</option>
+                        <option value="Lois">Lois Griffin</option>
+                        <option value="Chris">Chris Griffin</option>
+                        <option value="Meg">Meg Griffin</option>
+                        <option value="Stewie">Stewie Griffin</option>
+                    </optgroup>
+
+                    <optgroup label="Peter's Friends">
+                        <option value="Cleveland">Cleveland Brown</option>
+                        <option value="Joe">Joe Swanson</option>
+                        <option value="Quagmire">Glenn Quagmire</option>
+                    </optgroup>
+
+                    <option value="Evil Monkey">Evil Monkey</option>
+                    <option value="Herbert">John Herbert</option>
+                </select>
+
+                <div class="mb-3">
                     <label for="kode_katering" class="form-label">Kode Katering</label>
-                    <input type="text" name="kode_katering" class="form-control"  id="kode_katering">
+                    <input type="text" name="kode_katering" class="form-control" id="kode_katering">
                 </div>
                 <div class="mb-3">
                     <label class="form-label">Hari</label>
-                    <br/>
+                    <br />
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="hari[]"  value="senin">
-                        <label class="form-check-label" >Senin</label>
+                        <input class="form-check-input" type="checkbox" name="hari[]" value="senin">
+                        <label class="form-check-label">Senin</label>
                     </div>
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="checkbox" name="hari[]" value="selasa">
-                        <label class="form-check-label" >Selasa</label>
+                        <label class="form-check-label">Selasa</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="hari[]"  value="rabu">
-                        <label class="form-check-label" >Rabu</label>
+                        <input class="form-check-input" type="checkbox" name="hari[]" value="rabu">
+                        <label class="form-check-label">Rabu</label>
                     </div>
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="checkbox" name="hari[]" value="kamis">
-                        <label class="form-check-label" >Kamis</label>
+                        <label class="form-check-label">Kamis</label>
                     </div>
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="checkbox" name="hari[]" value="jumat">
-                        <label class="form-check-label" >Jumat</label>
+                        <label class="form-check-label">Jumat</label>
                     </div>
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="checkbox" name="hari[]" value="sabtu">
-                        <label class="form-check-label" >Sabtu</label>
+                        <label class="form-check-label">Sabtu</label>
                     </div>
                 </div>
                 <div class="mb-3">
@@ -72,7 +92,7 @@
                     <input type="text" name="harga_add_on" class="form-control" id="harga_add_on">
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
-            </form> 
+            </form>
         </div>
     </div>
 </div>
